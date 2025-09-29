f3760f5f958adcce845d8d1a13f6f661634a40eb
mickeysrishti <Mukeshpathak345@gmail.com>
2025-09-29 16:52:00 +0530

    Project 1 Code Updated

---
+++ Day 3: Project 1/main.js
@@ -0,0 +1,24 @@
+
+gsap.from(".logo", {
+    x: -100,
+    duration: 2
+})
+
+gsap.from(".menu", {
+    y: -100,
+    duration: 2,
+    delay: 1
+})
+
+gsap.from(".btn", {
+    x: 100,
+    duration: 2,
+    delay: 2
+})
+
+
+gsap.from("#page h1", {
+    x: "-150%",
+    duration: 3,
+    delay: 3
+})

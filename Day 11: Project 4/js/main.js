89d0864884da236fdef1903cb97c811cae328c82
mickeysrishti <Mukeshpathak345@gmail.com>
2025-10-08 17:39:08 +0530

    code udpated

--- Day 11: Project 4/js/main.js
+++ Day 11: Project 4/js/main.js
@@ -1,2 +1,52 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+function preloadImg(selector = "img") {
+    return new Promise((resolve) => {
+        imagesLoaded(document.querySelectorAll(selector), {
+            background: true
+        }, resolve)
+    });
+}
+
+
+const gridItems = Array.from(document.querySelectorAll(".grids > .grid"));
+
+preloadImg(".grid-img").then(() => {
+    const lenis = new Lenis({
+        lerp: 0.1,
+        smooth: true,
+    });
+    lenis.on("scroll", ScrollTrigger.update);
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    gridItems.forEach((item) => {
+        const image = item.querySelector(".grid-img");
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true
+            }
+        }).fromTo(image,
+            { scale: 0, opacity: 0, duration: 2, transformOrigin: "center center" },
+            { scale: 1.4, opacity: 1, duration: 2, ease: "none" }
+        )
+            .to(image, {
+                scale: 0, opacity: 0, duration: 2, ease: "none"
+            })
+    })
+})
+
+
+
+
+
+// Animate each grid 
+

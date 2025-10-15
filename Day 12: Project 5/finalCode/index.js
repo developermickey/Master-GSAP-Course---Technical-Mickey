516eaabc233d8ca9ae88848f293ef450ad3d6b52
mickeysrishti <Mukeshpathak345@gmail.com>
2025-10-16 01:36:22 +0530

    Day 12 Code added

---
+++ Day 12: Project 5/finalCode/index.js
@@ -0,0 +1,28 @@
+const page = document.querySelector(".page");
+const pageTitle = page.querySelector(".title");
+
+gsap
+  .timeline({
+    defaults: {
+      ease: "none",
+    },
+    scrollTrigger: {
+      trigger: page,
+      start: "clamp(top bottom)",
+      end: "bottom top",
+      scrub: true,
+    },
+  })
+  .to(page, {
+    yPercent: 35,
+    scale: 0.95,
+    startAt: { filter: "brightness(100%)" },
+    filter: "brightness(30%)",
+  })
+  .to(
+    pageTitle,
+    {
+      xPercent: -80,
+    },
+    0
+  );

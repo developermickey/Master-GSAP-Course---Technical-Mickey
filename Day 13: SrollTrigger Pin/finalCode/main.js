f4b5bda05a41919e594d64e1c18ffde10a2dda1e
mickeysrishti <Mukeshpathak345@gmail.com>
2025-10-16 18:49:40 +0530

    code updated

---
+++ Day 13: SrollTrigger Pin/finalCode/main.js
@@ -0,0 +1,78 @@
+let details = gsap.utils.toArray(".details");
+let props = gsap.getProperty("#id", "backgroundColor");
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.set(".photo:not(:first-child)", {
+  opacity: 1,
+  scale: 1,
+  clipPath: "inset(100% 0% 0%)",
+  scale: 1,
+});
+
+gsap.set(".photo img", {
+  y: 5,
+});
+
+const animation = gsap.to(".photo:not(:first-child)", {
+  opacity: 1,
+  scale: 1,
+  duration: 1,
+  stagger: 1,
+  clipPath: "inset(0% 0% 0%)",
+  scale: 1,
+  snap: true,
+});
+
+gsap.to(".photo img", {
+  y: -5,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+});
+
+ScrollTrigger.create({
+  trigger: ".gallery",
+  start: "top top",
+  end: "bottom bottom",
+  pin: ".right",
+  animation: animation,
+  scrub: 2,
+});
+
+gsap.to(".gallery", {
+  duration: 1,
+  backgroundColor: "#f9d2e5aa",
+  scrollTrigger: {
+    trigger: ".data1",
+    scrub: true,
+  },
+});
+
+gsap.to(".gallery", {
+  duration: 1,
+  backgroundColor: "#cdd1ffaa",
+  scrollTrigger: {
+    trigger: ".data2",
+    scrub: true,
+  },
+});
+
+gsap.to(".gallery", {
+  duration: 1,
+  backgroundColor: "#ffe4d3ee",
+  scrollTrigger: {
+    trigger: ".data3",
+    scrub: true,
+  },
+});
+
+gsap.to(".gallery", {
+  duration: 1,
+  backgroundColor: "#ffb399aa",
+  scrollTrigger: {
+    trigger: ".data4",
+    scrub: true,
+  },
+});
